d5386536021fbe88e07cadf010241117cacb103a
Alon Bar-Lev <alon.barlev@gmail.com>
2025-04-10 16:41:27 +0300

    linux-user-space-dma: build: xlnx_rebase_v6.6_LTS

--- linux-user-space-dma/Software/Kernel/dma-proxy.c
+++ linux-user-space-dma/Software/Kernel/dma-proxy.c
@@ -428,7 +428,7 @@ static int cdevice_init(struct dma_proxy_channel *pchannel_p, char *name)
 	 * in /dev to be created
 	 */
 	if (!local_class_p) {
-		local_class_p = class_create(THIS_MODULE, DRIVER_NAME);
+		local_class_p = class_create(DRIVER_NAME);
 
 		if (IS_ERR(pchannel_p->dma_device_p->class)) {
 			dev_err(pchannel_p->dma_device_p, "unable to create class\n");
